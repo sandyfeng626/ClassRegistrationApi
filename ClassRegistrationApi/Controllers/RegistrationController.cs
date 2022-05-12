2112586e7f28cb22a0993387da81fd3f8d279eb2
sandyfeng626 <105305522+sandyfeng626@users.noreply.github.com>
2022-05-12 12:11:27 -0400

    Initial commit

---
+++ ClassRegistrationApi/Controllers/RegistrationController.cs
@@ -0,0 +1,21 @@
+﻿using ClassRegistrationApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassRegistrationApi.Controllers;
+
+[Route("registrations")]
+[ApiController]
+public class RegistrationController : ControllerBase
+{
+
+    [HttpPost]
+    public async Task<ActionResult<Registration>> AddARegistration([FromBody] RegistrationRequest request)
+    {
+        
+        var response = new Registration("99", request);
+
+        return Ok(response);
+
+    }
+}
+// {name: 'Henry Gonzalez', dateOfCourse: '2022-06-07T00:00:00', course: '62797b1a1823357feb3756ac'}
